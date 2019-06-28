9103815cce372b26d0af2a732fff44658ec57c48
OldtimeFerryman <50560386+OldtimeFerryman@users.noreply.github.com>
2019-06-28 22:07:00 +0800

    Update factory.java

--- CreateClass/factory.java
+++ CreateClass/factory.java
@@ -5,3 +5,70 @@
 如何解决：让其子类实现工厂接口，返回的也是一个抽象的产品。
 关键代码：创建过程在其子类执行。
 */
+
+interface Cat {
+    void Meow();
+}
+class AmericanSharthaie implements Cat {
+
+    @Override
+    public void Meow() {
+        // TODO Auto-generated method stub
+        System.out.println("AmericanSharthaie meows!");
+    }
+
+}
+class Ragdoll implements Cat {
+
+    @Override
+    public void Meow() {
+        // TODO Auto-generated method stub
+        System.out.println("Ragdoll meows!");
+    }
+
+}
+class Munchkin implements Cat {
+
+    @Override
+    public void Meow() {
+        // TODO Auto-generated method stub
+        System.out.println("Munchkin meows!");
+    }
+
+}
+class CatFactory {
+
+    public static Cat getCat(String cat){
+        if(null == cat)
+            return null;
+        else if(cat.equalsIgnoreCase("Munchkin"))
+            return new Munchkin();
+        else if(cat.equalsIgnoreCase("AmericanSharthaie"))
+            return new AmericanSharthaie();
+        else if(cat.equalsIgnoreCase("Ragdoll"))
+            return new Ragdoll();
+        else
+            return null;
+    }
+}
+
+public class factory {
+    public static void main(String[] args){
+
+        Cat cat1 = CatFactory.getCat("Munchkin");
+        cat1.Meow();
+
+        Cat cat2 = CatFactory.getCat("AmericanSharthaie");
+        cat2.Meow();
+
+        Cat cat3 = CatFactory.getCat("Ragdoll");
+        cat3.Meow();
+    }
+}
+
+/* Out:
+ * Munchkin meows!
+ * AmericanSharthaie meows!
+ * Ragdoll meows!
+
+*/
