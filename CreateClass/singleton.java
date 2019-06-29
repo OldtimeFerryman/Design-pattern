0bdf303e5dd58076e5b5bd2493116a904a28b6ba
OldtimeFerryman <50560386+OldtimeFerryman@users.noreply.github.com>
2019-06-29 11:46:18 +0800

    Update singleton.java

--- CreateClass/singleton.java
+++ CreateClass/singleton.java
@@ -147,6 +147,34 @@ class singleton {
     }
 }
 
+// 5、登记式/静态内部类
+class singleton {
+    /**
+     * 是否 Lazy 初始化：是
+     * 是否多线程安全：是
+     * 对静态域使用延迟初始化，应使用这种方式而不是双检锁方式。
+     * 这种方式只适用于静态域的情况，双检锁方式可在实例域需要延迟初始化时使用。
+     *
+     * Singleton 类被装载了，instance 不一定被初始化。因为 SingletonHolder 类
+     * 没有被主动使用，只有通过显式调用 getInstance 方法时，才会显式装载
+     * SingletonHolder 类，从而实例化 instance。想象一下，如果实例化 instance
+     * 很消耗资源，所以想让它延迟加载，另外一方面，又不希望在 Singleton 类加载时就实例化，
+     * 因为不能确保 Singleton 类还可能在其他的地方被主动使用从而被加载，那么这个时候实例化 instance 显然是不合适的。
+     */
+    private static class singletonHolder {
+        private static final singleton INSTANCE = new singleton();
+    }
+
+    private singleton (){}
+    public static final singleton getInstance() {
+        return singletonHolder.INSTANCE;
+    }
+
+    public void showMessage(){
+        System.out.println("This is a singleton instance!");
+    }
+}
+
 // public class SingletonPatternDemo {
 //     public static void main(String[] args) {
 
