dc710c427aae6410d2b168604768793daf063311
OldtimeFerryman <50560386+OldtimeFerryman@users.noreply.github.com>
2019-06-30 09:55:49 +0800

    Create PrototypeDemo.java

---
+++ CreateClass/PrototypeDemo.java
@@ -0,0 +1,138 @@
+/**
+ * 用于创建重复的对象，同时又能保证性能。这种类型的设计模式属于创建型模式，它提供了一种创建对象的最佳方式。
+ *
+ * 这种模式是实现了一个原型接口，该接口用于创建当前对象的克隆。当直接创建对象的代价比较大时，则采用这种模式。
+ *
+ * 用原型实例指定创建对象的种类，并且通过拷贝这些原型创建新的对象。
+ *
+ * 使用场景： 1、资源优化场景。 2、类初始化需要消化非常多的资源，这个资源包括数据、硬件资源等。
+ * 3、性能和安全要求的场景。 4、通过 new 产生一个对象需要非常繁琐的数据准备或访问权限，则可以使用原型模式。
+ * 5、一个对象多个修改者的场景。 6、一个对象需要提供给其他对象访问，而且各个调用者可能都需要修改其值时，
+ * 可以考虑使用原型模式拷贝多个对象供调用者使用。
+ * 7、在实际项目中，原型模式很少单独出现，一般是和工厂方法模式一起出现，通过 clone 的方法创建一个对象，
+ * 然后由工厂方法提供给调用者。
+ *
+ * 使用原型模式复制对象不会调用类的构造方法。因为对象的复制是通过调用Object类的clone方法来完成的，
+ * 它直接在内存中复制数据，因此不会调用到类的构造方法。不但构造方法中的代码不会执行，甚至连访问权限都
+ * 对原型模式无效。
+ *
+ * 深拷贝与浅拷贝。Object类的clone方法只会拷贝对象中的基本的数据类型，对于数组、容器对象、引用对象等都不会拷贝，这就是浅拷贝。
+ * 如果要实现深拷贝，必须将原型模式中的数组、容器对象、引用对象等另行拷贝。
+ *
+ * 原型模式包含以下主要角色。
+ * 抽象原型类：规定了具体原型对象必须实现的接口。
+ * 具体原型类：实现抽象原型类的 clone() 方法，它是可被复制的对象。
+ * 访问类：使用具体原型类中的 clone() 方法来复制新的对象。
+ */
+
+import java.util.Hashtable;
+
+abstract class Shape implements Cloneable {
+
+    private String id;
+    protected String type;
+
+    abstract void draw();
+
+    public String getType(){
+        return type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Object clone() {
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
+}
+
+class Rectangle extends Shape {
+
+    public Rectangle(){
+        type = "Rectangle";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
+
+class Square extends Shape {
+
+    public Square(){
+        type = "Square";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
+
+class Circle extends Shape {
+
+    public Circle(){
+        type = "Circle";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
+
+class ShapeCache {
+
+    private static Hashtable<String, Shape> shapeMap
+            = new Hashtable<String, Shape>();
+
+    public static Shape getShape(String shapeId) {
+        Shape cachedShape = shapeMap.get(shapeId);
+        return (Shape) cachedShape.clone();
+    }
+
+    // 对每种形状都运行数据库查询，并创建该形状
+    // shapeMap.put(shapeKey, shape);
+    public static void loadCache() {
+        Circle circle = new Circle();
+        circle.setId("1");
+        shapeMap.put(circle.getId(),circle);
+
+        Square square = new Square();
+        square.setId("2");
+        shapeMap.put(square.getId(),square);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setId("3");
+        shapeMap.put(rectangle.getId(),rectangle);
+    }
+}
+
+public class PrototypeDemo {
+    
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+
+        Shape clonedShape = (Shape) ShapeCache.getShape("1");
+        System.out.println("Shape : " + clonedShape.getType());
+
+        Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+        System.out.println("Shape : " + clonedShape2.getType());
+
+        Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
+        System.out.println("Shape : " + clonedShape3.getType());
+    }
+    
+}
