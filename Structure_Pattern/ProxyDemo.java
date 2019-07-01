5bc8da6b65c5c225d824af4640d94125bc2dd030
OldtimeFerryman <50560386+OldtimeFerryman@users.noreply.github.com>
2019-07-01 09:34:28 +0800

    Update ProxyDemo.java

--- Structure_Pattern/ProxyDemo.java
+++ Structure_Pattern/ProxyDemo.java
@@ -1,1 +1,66 @@
+/**
+ * 由于某些原因需要给某对象提供一个代理以控制对该对象的访问。这时，访问对象不适合或者不能直接引用目标对象，
+ * 代理对象作为访问对象和目标对象之间的中介。
+ *
+ * 代理模式的主要优点有：
+ * 代理模式在客户端与目标对象之间起到一个中介作用和保护目标对象的作用；
+ * 代理对象可以扩展目标对象的功能；
+ * 代理模式能将客户端与目标对象分离，在一定程度上降低了系统的耦合度；
+ *
+ * 其主要缺点是：
+ * 在客户端和目标对象之间增加一个代理对象，会造成请求处理速度变慢；
+ * 增加了系统的复杂度；
+ */
 
+interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+
+    private String fileName;
+
+    public RealImage(String fileName){
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+
+    private void loadFromDisk(String fileName){
+        System.out.println("Loading " + fileName);
+    }
+}
+
+class ProxyImage implements Image{
+
+    private RealImage realImage;
+    private String fileName;
+
+    public ProxyImage(String fileName){
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if(realImage == null){
+            realImage = new RealImage(fileName);
+        }
+        realImage.display();
+    }
+}
+
+public class ProxyDemo {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test_10mb.jpg");
+
+        // 图像将从磁盘加载
+        image.display();
+        System.out.println("");
+        // 图像不需要从磁盘加载
+        image.display();
+    }
+}
