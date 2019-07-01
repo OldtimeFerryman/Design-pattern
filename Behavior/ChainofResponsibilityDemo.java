5b118ef4666c983b3658b1c1d3fd511287ef67a7
OldtimeFerryman <50560386+OldtimeFerryman@users.noreply.github.com>
2019-07-01 10:20:29 +0800

    Create ChainofResponsibilityDemo.java

---
+++ Behavior/ChainofResponsibilityDemo.java
@@ -0,0 +1,108 @@
+/**
+ * 每个接收者都包含对另一个接收者的引用。如果一个对象不能处理该请求，那么它会把相同的请求传给下一个接收者，依此类推。
+ *
+ * 何时使用：在处理消息的时候以过滤很多道。
+ *
+ * 如何解决：拦截的类都实现统一接口。
+ *
+ * 关键代码：Handler 里面聚合它自己，在 HandlerRequest 里判断是否合适，如果没达到条件则向下传递，向谁传递之前 set 进去。
+ *
+ * 1、有多个对象可以处理同一个请求，具体哪个对象处理该请求由运行时刻自动确定。
+ * 2、在不明确指定接收者的情况下，向多个对象中的一个提交一个请求。
+ * 3、可动态指定一组对象处理请求。
+ *
+ * 责任链模式是一种对象行为型模式，其主要优点如下。
+ * 降低了对象之间的耦合度。该模式使得一个对象无须知道到底是哪一个对象处理其请求以及链的结构，发送者和接收者也无须拥有对方的明确信息。
+ * 增强了系统的可扩展性。可以根据需要增加新的请求处理类，满足开闭原则。
+ * 增强了给对象指派职责的灵活性。当工作流程发生变化，可以动态地改变链内的成员或者调动它们的次序，也可动态地新增或者删除责任。
+ * 责任链简化了对象之间的连接。每个对象只需保持一个指向其后继者的引用，不需保持其他所有处理者的引用，这避免了使用众多的 if 或者 if···else 语句。
+ * 责任分担。每个类只需要处理自己该处理的工作，不该处理的传递给下一个对象完成，明确各类的责任范围，符合类的单一职责原则。
+ *
+ * 其主要缺点如下。
+ * 不能保证每个请求一定被处理。由于一个请求没有明确的接收者，所以不能保证它一定会被处理，该请求可能一直传到链的末端都得不到处理。
+ * 对比较长的职责链，请求的处理可能涉及多个处理对象，系统性能将受到一定影响。
+ * 职责链建立的合理性要靠客户端来保证，增加了客户端的复杂性，可能会由于职责链的错误设置而导致系统出错，如可能会造成循环调用。
+ */
+
+abstract class AbstractLogger {
+    public static int INFO = 1;
+    public static int DEBUG = 2;
+    public static int ERROR = 3;
+
+    protected int level;
+
+    //责任链中的下一个元素
+    protected AbstractLogger nextLogger;
+
+    public void setNextLogger(AbstractLogger nextLogger){
+        this.nextLogger = nextLogger;
+    }
+
+    public void logMessage(int level, String message){
+        if(this.level == level){
+            write(message);
+        }
+        if(nextLogger !=null){
+            nextLogger.logMessage(level, message);
+        }
+    }
+
+    abstract protected void write(String message);
+
+}
+
+class ConsoleLogger extends AbstractLogger {
+
+    public ConsoleLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+}
+
+class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
+
+class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
+
+
+public class ChainofResponsibilityDemo {
+    public static void main(String[] args) {
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);// 3
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);// 2
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);// 1
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+//        error -> file -> console
+        AbstractLogger loggerChain = errorLogger;
+        loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG, "This is a debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR, "This is an error information.");
+    }
+}
